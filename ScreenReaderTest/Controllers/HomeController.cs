326c7f1abfd03ce9c8753de963ea06a35bcc2907
Vicky Howells <vicky.howells@method4.co.uk>
2020-04-19 14:25:28 +0100

    Original demo from 16/04/2020

---
+++ ScreenReaderTest/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ScreenReaderTest.Models;
+
+namespace ScreenReaderTest.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        public IActionResult DocumentStructure() {
+	        return View();
+        }
+
+        public IActionResult Navigation() {
+	        return View();
+        }
+
+        public IActionResult Tables() {
+	        return View();
+        }
+
+        public IActionResult Forms() {
+	        return View();
+        }
+
+        public IActionResult FormSuccess() {
+	        return View();
+        }
+
+        public IActionResult Content() {
+	        return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ScreenReaderTest/wwwroot/js/validation.js
@@ -0,0 +1,90 @@
+﻿var errors = document.getElementById('errors');
+var form = document.getElementById('contact');
+var required = Array.prototype.slice.call(form.querySelectorAll('[required]'));
+
+//form.addEventListener('blur', function (e) {
+
+//    if (e.target.hasAttribute('required')) {
+//        validate(e.target);
+//    }
+
+//}, true);
+
+//form.addEventListener('submit', function (e) {
+//    e.preventDefault();
+//    var invalid = [];
+
+//    required.forEach(function (field) {
+
+//        if (validate(field)) {
+//            invalid.push(field);
+//        }
+//    });
+
+//    if (invalid.length) {
+//        var list = '<h2>Errors</h2><ol id="error-list">';
+
+//        invalid.forEach(function (field) {
+//            list += '<li><a href="#' + field.id + '">' +
+//                field.previousElementSibling.querySelector('.error-message')
+//                .textContent + '</a></li>';
+//        });
+
+//        list += '</ol>';
+
+//        errors.innerHTML = list;
+//        errors.focus();
+//    } else {
+//        errors.innerHTML = '';
+//    }
+//});
+
+//function ready(func) {
+//    document.addEventListener("DOMContentLoaded", func);
+//}
+
+//ready(() => {
+//    bindBadFormSubmit();
+//});
+
+
+//function bindBadFormSubmit() {
+//    $('#badForm').click(function() {
+//        validate();
+//    });
+//}
+
+$(function () {
+    $("#badForm").submit(function (event) {
+        event.preventDefault();
+        validate();
+    });
+});
+
+function validate() {
+    var firstName = document.forms["badForm"]["fname"].value;
+    var surName = document.forms["badForm"]["sname"].value;
+    var emailAddress = document.forms["badForm"]["email"].value;
+
+    var valid = true;
+
+    if (firstName === "") {
+        document.forms["badForm"]["fname"].classList.add("invalid");
+        document.forms["badForm"].event.preventDefault();
+        valid = false;
+    }
+
+    if (surName === "") {
+        document.forms["badForm"]["sname"].classList.add("invalid");
+        document.forms["badForm"].event.preventDefault();
+        valid = false;
+    }
+
+    if (emailAddress === "") {
+        document.forms["badForm"]["email"].classList.add("invalid");
+        document.forms["badForm"].event.preventDefault();
+        valid = false;
+    }
+
+    return valid;
+}
